78f36fd31b70d62693833eeab9b0a3f64997a066
Jaime Torrejón Velásquez <jaime.t.velasquez@gmail.com>
2021-03-30 11:12:04 +0200

    adding play route to router

--- src/app/routers/app-routing.module.ts
+++ src/app/routers/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { CourseDetailComponent } from '../components/course-detail/course-detail.component';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
+import { PreplayScreenComponent } from '../components/preplay-screen/preplay-screen.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'course-detail/:id', component: CourseDetailComponent },
+  { path: 'play/:id', component: PreplayScreenComponent}
 ];
 
 
